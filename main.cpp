7acd508ff8d53e6ab1186db814c6a2b54c3ad265
Kolya <kolyazoloto@mail.ru>
2019-03-25 20:50:10 +0800

    unsigned char

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@
 
 
 class LED_Strip{
-	public:
+public:
 	LED_Strip(int length){
 		DDRB |= (1<<PB0);
 		strip_length=length;
-		temp = (int**)malloc(strip_length*sizeof(int*)); //sozdaem massive
+		temp = (unsigned char**)malloc(strip_length*sizeof(unsigned char*)); //sozdaem massive
 		for(unsigned char i=0;i<strip_length;i++){
-			temp[i] = (int*)malloc(3*sizeof(int));
+			temp[i] = (unsigned char*)malloc(3*sizeof(unsigned char));
 		}
 	}
 	void LED_color(unsigned char red,unsigned char green,unsigned char blue)
@@ -30,25 +30,25 @@ class LED_Strip{
 		}
 
 	}
-	void LED_grad(int red1,int green1,int blue1,int red2,int green2,int blue2){
-		int mean_red = (red1-red2)/(strip_length-1);
-		int mean_green = (green1-green2)/(strip_length-1);
-		int mean_blue = (blue1-blue2)/(strip_length-1);
-		int mean[3] = {mean_green,mean_red,mean_blue};
+	void LED_grad(unsigned char red1,unsigned char green1,unsigned char blue1,unsigned char red2,unsigned char green2,unsigned char blue2){
+		int step_red = (red1-red2)/(strip_length-1);
+		int step_green = (green1-green2)/(strip_length-1);
+		int step_blue = (blue1-blue2)/(strip_length-1);
+		int step[3] = {step_green,step_red,step_blue};
 		int first_color[3] = {green1,red1,blue1};
-		for (int i=0; i<strip_length; i++){
-			for (int j=0;j<3;j++)
+		for (unsigned char i=0; i<strip_length; i++){
+			for (unsigned char j=0;j<3;j++)
 			{
-				temp[i][j]=first_color[j]-mean[j]*i;//na kajdii shag izmenyaem cvet
+				temp[i][j]=first_color[j]-step[j]*i;//na kajdii shag izmenyaem cvet
 			}
 		}
 	}
 	
-	void LED_2color_string(int red1,int green1,int blue1,int red2,int green2,int blue2,int effect_length){
+	void LED_2color_string(unsigned char red1,unsigned char green1,unsigned char blue1,unsigned char red2,unsigned char green2,unsigned char blue2,unsigned char effect_length){
 		int effect_color[3] = {green2,red2,blue2};
 		int strip_color[3] = {green1,red1,blue1};
-		for (int i=0; i<strip_length; i++){
-			for (int j=0;j<3;j++){
+		for (unsigned char i=0; i<strip_length; i++){
+			for (unsigned char j=0;j<3;j++){
 				if(i<effect_length){
 					temp[i][j] = effect_color[j];
 				}
@@ -59,23 +59,23 @@ class LED_Strip{
 		}
 		LED_Write();
 	}
-	void LED_dwig_string(int effect_length)
+	void LED_dwig_string(unsigned char effect_length)
 	{
-		for(int i=0;i<strip_length-effect_length;i++)
+		for(unsigned char i=0;i<strip_length-effect_length;i++)
 		{
 			right_sdwig();
 			LED_Write();
-			_delay_ms(100);
+			_delay_ms(10);
 		}
-		for(int i=0;i<strip_length-effect_length;i++)
+		for(unsigned char i=0;i<strip_length-effect_length;i++)
 		{
 			left_sdwig();
 			LED_Write();
-			_delay_ms(100);
+			_delay_ms(10);
 		}
 		
 	}
-	void LED_time_grad(int red1,int green1,int blue1,int red2,int green2,int blue2){
+	void LED_time_grad(unsigned char red1,unsigned char green1,unsigned char blue1,unsigned char red2,unsigned char green2,unsigned char blue2){
 		int mean_red = (red1-red2)/(strip_length-1);
 		int mean_green = (green1-green2)/(strip_length-1);
 		int mean_blue = (blue1-blue2)/(strip_length-1);
@@ -90,10 +90,10 @@ class LED_Strip{
 				LED_color(red2,green2,blue2);
 				LED_Write();
 			}
-			for(int k=0;k<100-1;k++){
+			for(unsigned char k=0;k<100-1;k++){
 				_delay_ms(100);
-				for (int i=0; i<100; i++){
-					for (int j=0;j<3;j++){
+				for (unsigned char i=0; i<100; i++){
+					for (unsigned char j=0;j<3;j++){
 						temp[i][j] -= sign * mean[j];
 					}
 				}
@@ -104,8 +104,8 @@ class LED_Strip{
 	}
 
 	void LED_Write(void){
-		int a;
-		int j,i,n;
+		unsigned char a;
+		unsigned char j,i,n;
 		for (n=0; n<strip_length; n++)
 		{
 			for (j=0; j<3; j++)			//ïåðâûì âûñòàâëÿåòñÿ G (Hi->Low), ïîòîì R è B
@@ -127,46 +127,28 @@ class LED_Strip{
 		}
 		stop_cycle(); //îêîí÷àíèå öèêëà
 	}
-	void LED_rand(){
-		for (int i=0; i<strip_length; i++)
-		for (int j=0;j<3;j++)
-		temp[i][j]=rand() % 256;
-		LED_Write();
-		_delay_ms(100);
 
-	}
-
-	void operator>>(int a){
+	void operator>>(unsigned char a){
 		for(int i=0;i<a;i++){
 			right_sdwig();
 		}
 	}
-	void operator<<(int a){
+	void operator<<(unsigned char a){
 		for(int i=0;i<a;i++){
 			left_sdwig();
 		}
 	}
-	void color_single(int number,int r,int g,int b){
+	void color_single(unsigned char number,unsigned char r,unsigned char g,unsigned char b){
+		unsigned char color[3] = {g,r,b};
 		for(int i=0;i<3;i++){
-			switch (i)
-			{
-			case 0:
-				temp[number-1][i] = g;
-				break;
-			case 1:
-				temp[number-1][i] = r;
-				break;
-			case 2:
-				temp[number-1][i] = b;
-				break;
-			}
+			temp[number-1][i] = color[i] ;
 		}
 	}
 	
 
-	private:
-	int strip_length;
-	int **temp;
+private:
+	unsigned char strip_length;
+	unsigned char **temp;
 	
 	void one(){
 		PORTB |= (1<<PB0);
@@ -197,8 +179,8 @@ class LED_Strip{
 	}
 	void left_sdwig(){
 		int first[3]={temp[0][0],temp[0][1],temp[0][2]};
-		for (int i=0; i<strip_length-1; i++){
-			for (int j=0;j<3;j++)
+		for (unsigned char i=0; i<strip_length-1; i++){
+			for (unsigned char j=0;j<3;j++)
 			{
 				temp[i][j] = temp[i+1][j];
 			}
@@ -214,13 +196,11 @@ class LED_Strip{
 
 
 int main(void){
-	LED_Strip strip(20); //118
-	strip.LED_color(0,0,0);
-	strip.color_single(3,0,0,255);
-
+	LED_Strip strip(118); //118
+	strip.LED_2color_string(255,0,0,255,255,0,12);
 	strip.LED_Write();
 	while (1){
-
+		strip.LED_dwig_string(12);
 	}
 	
 }
